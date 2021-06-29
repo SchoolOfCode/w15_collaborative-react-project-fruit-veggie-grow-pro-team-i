6fba6c58206440dfe1da691b770417a78a6521b4
Jordan Linton <jordaneddielinton93@hotmail.co.uk>
2021-06-29 12:49:16 +0100

    Added All the appropriate files and folders except the components

--- veggie_fruit_growpros/src/App.js
+++ veggie_fruit_growpros/src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
+import BrowsePage from './components/BrowsePage/BrowsePage';
+
+import "./components/BrowsePage/BrowsePage.css"
 import './App.css';
 
 function App() {
+
+// we dont need to work here untill we add Landingpage&Questionaire
+
+
   return (
     <div className="App">
-      
+      <BrowsePage/>
     </div>
   );
 }
---
+++ veggie_fruit_growpros/src/components/BrowsePage/BrowsePage.js
@@ -0,0 +1,15 @@
+const BrowsePage = () => {
+  // THIS IS WHERE WE WILL BE WORKING
+  
+
+
+  return ( 
+    <div className="BrowsePage">
+    {/* components go in the BrowsePage div */}
+    Hello Team
+    
+    </div>
+   );
+}
+ 
+export default BrowsePage;
