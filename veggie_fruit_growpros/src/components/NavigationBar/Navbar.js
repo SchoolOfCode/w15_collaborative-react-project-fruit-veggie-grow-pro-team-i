d370b2df142db4c8886e678ecf36da38eeeacbc3
Jack Browne <jackbrowne00@gmail.com>
2021-07-05 19:39:51 +0100

    questionnaire page added, functional buttons working

--- veggie_fruit_growpros/src/components/NavigationBar/Navbar.js
+++ veggie_fruit_growpros/src/components/NavigationBar/Navbar.js
@@ -1,21 +1,20 @@
-import React, {Component}  from 'react';
 import { MenuItems } from "./MenuItems"
 import './Navbar.css'
 
 
-const Navbar = () => {
+const Navbar = ({changePageFunction}) => {
     return (
         <>
         <div className="color-border"></div> 
         <nav className="NavbarItems">
-            <h1 className="navbar-logo">Logo<a href='#' className="navbar-icon" ></a></h1>
+            <h1 className="navbar-logo">Logo</h1>
             <ul className="nav-menu">
                 {MenuItems.map((item, index) => {
                     return (
                         <li key={index}>
-                            <a className={item.className} href={item.link}>
+                            <h5 onClick={() => {changePageFunction(item.function)}} className={item.className}>
                             {item.title}
-                            </a>
+                            </h5>
                         </li>
                     )
                 })}
