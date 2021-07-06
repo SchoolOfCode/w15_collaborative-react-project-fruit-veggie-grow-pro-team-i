ba253d7d199d7b837dff05c6fc68ece3563b6338
Jack Browne <jackbrowne00@gmail.com>
2021-07-06 01:46:20 +0100

    styling nav bar

--- veggie_fruit_growpros/src/components/NavigationBar/Navbar.js
+++ veggie_fruit_growpros/src/components/NavigationBar/Navbar.js
@@ -5,9 +5,9 @@ import './Navbar.css'
 const Navbar = ({changePageFunction}) => {
     return (
         <>
+        <header className="Nav-Conatainer">
         <div className="color-border"></div> 
         <nav className="NavbarItems">
-        <div className="NavbarSpacer"></div>
             <h1 className="navbar-logo">Logo</h1>
             <ul className="nav-menu">
                 {MenuItems.map((item, index) => {
@@ -16,29 +16,11 @@ const Navbar = ({changePageFunction}) => {
                             <h5 onClick={() => {changePageFunction(item.function)}} className={item.className}>
                             {item.title}
                             </h5>
-                            
                         </li>
                     )
                 })}
             </ul>
         </nav>
-
-        <header className="Nav-Conatainer">
-
-            <nav className="NavbarItems">
-                <h1 className="navbar-logo">Logo</h1>
-                <ul className="nav-menu">
-                    {MenuItems.map((item, index) => {
-                        return (
-                            <li key={index}>
-                                <h5 onClick={() => {changePageFunction(item.function)}} className={item.className}>
-                                {item.title}
-                                </h5>
-                            </li>
-                        )
-                    })}
-                </ul>
-            </nav>
         </header>
         </>
     );
