cf739f071b3a5029c7f4fdd8c871c1e8fff92ab2
Jamie Hannah <jamiehannah1047@gmail.com>
2021-07-09 13:33:54 +0100

    Added herb information. All item card information has been added to our project

--- veggie_fruit_growpros/src/components/BrowsePage/popup/PopUp.js
+++ veggie_fruit_growpros/src/components/BrowsePage/popup/PopUp.js
@@ -276,188 +276,179 @@ function PopUp({ popUpID, setpopUpID }) {
       ]
       break
     case "Lime":
-      paragraph = "	Limes are the fruit of a variety of hybrid citrus trees. There are many species of limes and they do not share a common parent because the Citrus genus hybridizes so easily. Species include Key Lime (Citrus aurantifolia), Australian Desert Lime (Citrus glauca), Persian Lime (Citrus × latifolia) and Kaffir Lime (Citrus hystrix). Limes are generally ellipsoid, lime green, 3-6cm in diameter, and have acidic, tangy, sour juices. The Persian Lime is the most widely produced lime in the world. Most limes cannot tolerate temperatures below 10° C. More growing information is available in individual lime species entries."
-      link = "https://en.wikipedia.org/wiki/Lime_(fruit)"
+      paragraph = "Citrus are not hardy in Britain but can be grown in pots outdoors in summer and brought inside for winter. Of all citrus, most gardeners grow lemons; kumquats are the most cold tolerant; others, like limes and grapefruits, need more warmth. The fragrant flowers can appear all year round, but are especially abundant in late winter. Fruit ripens up to 12 months later, so they often flower and fruit at the same time."
+      link = "https://www.rhs.org.uk/advice/grow-your-own/fruit/citrus"
       sowTitle = "Sowing"
       sowImage = sowingImg
       sowDescription = [
-        "",
+        "Citrus need a bright sunny position. Most only reach 1–1.5m (3–5ft) tall in a pot but can grow larger in good conditions, depending on the rootstock.",
         <br />, 
         <br />,
-        "",
-        <br />, 
-        <br />,
-        ""
+        "They are better grown in pots in a cool climate so they can be moved indoors. Although any good potting medium will do, a soil-based compost such as John Innes No 2 is best. Add up 20 per cent sharp sand or grit. There are also specially formulated citrus composts available."
       ]
       growTitle = "Growing"
       growImage = growingImg
       growDescription = [
-        "",
+        "Citrus in pots can be put outdoors in summer, in a sheltered sunny position, but only when temperatures increase, from mid-June until late September. Keep some fleece handy in case of sudden cold nights in early summer. Low temperatures will inhibit flowering and may cause damage or even death.",
         <br />, 
         <br />,
-        "",
+        "A minimum winter night temperature of 10°C (50°F) is needed for lemons and limes. Calamondin oranges need a minimum winter night temperature of 13°C (55°F).",
         <br />, 
         <br />,
-        ""
+        "Kumquats are unusual citrus, as the fruits are eaten whole – including the skin. Plants are naturally very bushy and highly productive. They can tolerate winter temperatures down to 7°C (45°F) – among the hardiest of all citrus."
       ]
       harvestTitle = "Harvesting"
       harvestImage = harvestingImg
       harvestDescription = [
-        "",
-        <br />, 
-        <br />,
-        "",
-        <br />, 
-        <br />,
-        ""
+        "Once citrus fruits are fully grown, they develop a rich skin colour. They can then either be picked or left on the tree until needed.",
       ]
       break
     case "Basil":
-      paragraph = "Fragrant and delicious herb with tender green leaves. Great in just about anything - salad, sauces, meat marinades, and even popcorn."
-      link = "https://en.wikipedia.org/wiki/Basil"
+      paragraph = "This popular annual herb is an essential ingredient in Italian cooking with strongly flavoured leaves that can be used to perk up tomato dishes or blitzed to make pesto sauce. Seeds are best sown indoors in late winter or early spring for plants that can be grown outdoors in summer, providing leaves that can harvested well into the autumn. "
+      link = "https://www.rhs.org.uk/advice/grow-your-own/herbs/basil"
       sowTitle = "Sowing"
       sowImage = sowingImg
       sowDescription = [
-        "",
+        "Seeds are best started off indoors from late February to mid-summer. Fill a 7.5cm (3in) pot with seed compost, firm down and sow a few seeds of basil over the top – most will germinate so only sow a few more seeds than you need. Cover with a thin layer of vermiculite, water gently and pop it into a propagator. If you don’t have one, don’t worry. You can cover the pot with a small, clear freezer bag and secure with an elastic band.",
         <br />, 
         <br />,
-        "",
-        <br />, 
-        <br />,
-        ""
+        "After germination, remove from the propagator (or take the bag off) and keep damp. When the seedlings are large enough to handle and have developed their first true leaves (ignore the rounded seed leaves), give them their own 7.5cm (3in) pot filled with multi-purpose compost. "
       ]
       growTitle = "Growing"
       growImage = growingImg
       growDescription = [
-        "",
+        "Plant basil outside after all danger of frost has passed, choosing a sunny, sheltered spot with well-drained soil or grow plants in containers. You can keep a plant in a pot going all summer long by moving it into a slightly bigger container every time roots show through the drainage holes in the bottom – plants could end up in a 20cm (8in) container.",
         <br />, 
         <br />,
-        "",
+        "Basil hates having wet roots overnight, so aim to water plants in the morning if possible.",
         <br />, 
         <br />,
-        ""
+        "Keep plants bushy and productive by pinching the tips of branches regularly and remove any flowers that start to develop. "
       ]
       harvestTitle = "Harvesting"
       harvestImage = harvestingImg
       harvestDescription = [
-        "",
+        "Basil leaves can be harvested throughout the summer.",
         <br />, 
         <br />,
-        "",
-        <br />, 
-        <br />,
-        ""
+        "Remove leaves as required or harvest entire plants if lots of leaves are needed to make pesto or sauce. If only a few leaves are required, remove the tops of plants to encourage bushy growth. ",
       ]
       break
     case "Mint":
-      paragraph = "Mint is a perennial herb with a distinctive taste. It's stems are square-shaped and it has pink, purple, or white flowers. Peppermint, ginger mint, and large apple mint are hybrids of mint. Mint is often grown in pots to prevent it from overtaking the garden with its invasive, spreading rhizome root structure. It's leaves are most aromatic before the plant flowers and can be used fresh, dried, or frozen. Mint's strongly scented leaves confuse the pests of carrots, tomatoes, alliums, and brassicas, and deter flea beetles."
-      link = "https://en.wikipedia.org/wiki/Lamiaceae"
+      paragraph = "Mint is a perennial herb grown for its leaves. They are wonderful infused in hot water to make a refreshing tea, chopped and added to many dishes, or used to make mint sauce (a classic addition to roast lamb). There are many different varieties of mint to choose from with leaves that smell completely different. Not all are good for culinary use, so choose what you grow carefully. "
+      link = "https://www.rhs.org.uk/advice/grow-your-own/herbs/mint"
       sowTitle = "Sowing"
       sowImage = sowingImg
       sowDescription = [
-        "",
+        "It is best to buy mint as young plants in spring. Mint is a vigorous plant that will spread all over the place if planted straight into the ground. This is why it is a good idea to plant it in a large pot filled with multi-purpose compost that can be placed in a prominent place to make picking easy.",
         <br />, 
         <br />,
-        "",
-        <br />, 
-        <br />,
-        ""
+        "Alternatively grow in a large, bottomless bucket and plunge it into a gap in the soil, making sure the lip of the container remains above the surface to prevent shoots from escaping over the top. ",
       ]
       growTitle = "Growing"
       growImage = growingImg
       growDescription = [
-        "",
+        "Give plants plenty of water, especially during hot, dry weather.",
         <br />, 
         <br />,
-        "",
+        "When plants have finished flowering in summer, cut flowered shoots back to 5cm (2in) above the surface of the compost.",
         <br />, 
         <br />,
-        ""
+        "Avoid growing different varieties of mint close together, whether in pots or the ground, as they can lose their individual scent and flavour.",
+        <br />, 
+        <br />,
+        "When growing in pots, rejuvenate congested clumps by upturning the container, removing the rootball and splitting it in half. Repot a portion in the same container using fresh compost. "
       ]
       harvestTitle = "Harvesting"
       harvestImage = harvestingImg
       harvestDescription = [
-        "",
+        "Mint can be harvested from late spring and mid-autumn, before it dies back over winter.",
         <br />, 
         <br />,
-        "",
+        "Pick regularly to keep plants compact and ensure they produce lots of fresh new shoots.",
         <br />, 
         <br />,
-        ""
+        "The leaves are best used fresh, but you can also freeze them for use in winter. Chop the leaves finely and pack into an ice-cube tray with some water, then freeze. Whenever you need some mint for a recipe, simply knock out as many ice cubes as you want and add to the pan. "
       ]
       break
     case "Parsley":
       paragraph = "	Parsley is an herb in the Apiaceae family with two main cultivars: flat leafed (or Italian) and curly. Some gardeners feel flat leaf is easier to cultivate since it is more tolerant of rain and sunshine. Curly parsley is more decorative in appearance. Both cultivars can be used fresh or dried to season food."
-      link = "https://en.wikipedia.org/wiki/Parsley"
+      link = "https://www.rhs.org.uk/advice/grow-your-own/herbs/parsley"
       sowTitle = "Sowing"
       sowImage = sowingImg
       sowDescription = [
-        "",
+        "Sow outdoors from early spring to the start of summer in well-drained soil in sun or partial shade. Sow seeds in shallow, 1cm ½in) deep trenches. Cover the trench and water. When they are large enough to handle, thin seedlings to 15cm (6in) apart with 15cm (6in) between rows.",
         <br />, 
         <br />,
-        "",
-        <br />, 
-        <br />,
-        ""
+        "Alternatively, grow in pots. Sow seeds thinly across a 25cm (10in) pot filled with seed compost, cover with a 1cm (½in) layer of compost and water. Leave in a cool spot to germinate and make sure the compost doesn’t dry out. Germination can take up to six weeks, then when they are large enough to handle, thin out seedlings, leaving about 2cm (¾in) between plants. ",
       ]
       growTitle = "Growing"
       growImage = growingImg
       growDescription = [
-        "",
+        "Keep plants well watered, especially during hot, dry spells in summer.",
         <br />, 
         <br />,
-        "",
+        "Give plants a boost by feeding every few weeks with a balanced liquid fertiliser.",
         <br />, 
         <br />,
-        ""
+        "Remove flowerheads to extend the cropping life of the plants.",
+        <br />, 
+        <br />,
+        "Prevent plants from becoming unsightly and encourage new growth, by snipping off any lower shoots that start to turn yellow. "
       ]
       harvestTitle = "Harvesting"
       harvestImage = harvestingImg
       harvestDescription = [
-        "",
+        "Parsley can be harvested throughout the summer months. Take a few leafy stems from the outside of a clump, snipping them near the base with scissors.",
         <br />, 
         <br />,
-        "",
+        "The leaves are best used fresh, but can also be frozen or dried for use in winter.",
         <br />, 
         <br />,
-        ""
+        "To freeze, chop the leaves and add to an ice-cube tray, then top up with water and freeze. You can then simply add the cubes to your cooking whenever needed.",
+        <br />, 
+        <br />,
+        "To dry, hang up a bunch of parsley in a warm, dark, well-ventilated place for a few weeks. When fully dried, crush the leaves and store in an air-tight jar. "
       ]
       break
     case "Dill":
-      paragraph = "Dill is an herb wth slender green stems and feathery, delicate leaves. It's flowers are white to yellow. Fresh and dried dill leaves are used to season fish, borscht, and to make pickles. Dill is best when used fresh. It loses its flavor rapidly if dried. Dill seed is also used as a spice."
-      link = "https://en.wikipedia.org/wiki/Dill"
+      paragraph = "Dill is a large annual or biennial herb with ferny foliage that is topped with sprays of yellow flower in summer. The strongly flavoured leaves can be chopped into soups, salads or used to flavour rice. Seeds are often used as ingredient in curry powder. This statuesque plant is ideally suited to growing in the middle of a border, its leaves providing a feathery foil for other plants. "
+      link = "https://www.rhs.org.uk/advice/grow-your-own/herbs/dill"
       sowTitle = "Sowing"
       sowImage = sowingImg
       sowDescription = [
-        "",
+        "Dill hates having its roots being disturbed or being transplanted, so sow in-situ, either into pots or the ground where it is to grow.",
         <br />, 
         <br />,
-        "",
+        "Start dill off from seed anytime between mid-spring and mid-summer. Prepare the soil well, choosing a fertile, open site in full sun. Sow seeds thinly in shallow, 1cm (½in) deep, rows and cover lightly with soil. Thin seedlings when large enough to handle to 15cm (6in) apart.",
         <br />, 
         <br />,
-        ""
+        "Alternatively, sow seeds thinly in large pots filled with multi-purpose compost, including peat free media. Thin seedlings to 10cm (4in) apart when large enough to handle. "
       ]
       growTitle = "Growing"
       growImage = growingImg
       growDescription = [
-        "",
+        "Do not allow soil or compost to dry out. Water plants regularly, especially during hot, dry summers, but do not over water. ",
         <br />, 
         <br />,
-        "",
+        "Support plants with garden canes or twiggy sticks to prevent them toppling over in a gust of wind.",
         <br />, 
         <br />,
-        ""
+        "Avoid growing dill near fennel, as the two can cross breed resulting in undesirable seedlings with poor flavour.",
+        <br />, 
+        <br />,
+        "Hoe around plants to prevent weeds from competing or smothering the growth of dill. "
       ]
       harvestTitle = "Harvesting"
       harvestImage = harvestingImg
       harvestDescription = [
-        "",
+        "Cut the leaves as required during spring and summer. Picking young leaves regularly will help to keep plants productive and delay flowering. The leaves can be used fresh, or can be either frozen or dried for later use.",
         <br />, 
         <br />,
-        "",
+        "To dry dill leaves, hang up sprigs in a dark, well-ventilated place for a few weeks. When fully dried, strip the leaves from the stems and store in an air-tight jar.",
         <br />, 
         <br />,
-        ""
+        "The seeds can be gathered in late summer when they start to ripen and turn brown – cut whole stems and put the seedhead in a paper bag, then hang the stems upside down until the seeds dry and drop. Remove any bits of stem, then store the seeds in an air-tight container. The seeds can be used ground or whole. "
       ]
       break
 
